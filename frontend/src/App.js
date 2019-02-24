df0222da81677d17afc1702f5a08f9c70c0f0c16
Amanpreet Singh <asg@fb.com>
2019-02-25 00:02:02 +0530

    Fix imports, add base url, and relative urls on github pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import Header from './components/Header';
-import ExploreNew from './components/Explore_new';
+import Explore from './components/Explore';
 import Home from './components/Home';
 
-import logo from './logo.svg';
 import './App.css';
 import theme from './styles';
 
@@ -16,14 +14,14 @@ class App extends Component {
   render() {
     return (
       <MuiThemeProvider theme={theme}>
-        <Router>
+        <Router basename={process.env.PUBLIC_URL + '/'}>
           <div className="App">
             <header className="App-header">
               <Header/>
             </header>
               <div>
                 <Route path="/" exact component={Home} />
-                <Route path="/explore" component={ExploreNew} />
+                <Route path="/explore" component={Explore} />
               </div>
           </div>
         </Router>
