f4911401368a5d67cac9a6dccc6b2adaa9104c17
Amanpreet Singh <asg@fb.com>
2019-03-01 11:36:07 -0800

    Add logo to header and more mobile friendly

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,11 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types'
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import { withStyles } from '@material-ui/core/styles';
 import Link from '@material-ui/core/Link';
-import theme from '../styles'
+import theme from '../styles';
 
 const styles = {
     root: {
@@ -19,7 +18,17 @@ const styles = {
     buttonLink: {
         color: theme['palette']['primary']['contrastText'],
         textDecoration: 'none'
-    }
+    },
+    buttonsSide: {
+        textAlign: 'right'
+    },
+    logo: {
+        height: '50px',
+        verticalAlign: 'middle',
+        [theme.breakpoints.down('sm')]: {
+            height: '35px'
+        }
+    },
   };
 
 function Header(props) {
@@ -29,18 +38,32 @@ function Header(props) {
         <div className={classes.root}>
         <AppBar position="static" color="primary">
             <Toolbar>
-            <Typography variant="h6" color="inherit" align="left" className={classes.grow}>
                 <Link
                     underline="none"
-                    className={classes.buttonLink}
+                    align="left"
+                    className={[classes.buttonLink, classes.grow].join(' ')}
                     href={process.env.PUBLIC_URL + '/'}
-                >
-                    TextVQA
+                    >
+                    <img
+                        className={classes.logo}
+                        srcSet="assets/images/textvqa_logo_and_text_white.svg"
+                        alt="TextVQA"
+                        />
                 </Link>
-            </Typography>
-            <Link underline="none" className={classes.buttonLink} href="explore">
-                <Button color="inherit">Explore</Button>
-            </Link>
+                <Link
+                    underline="none"
+                    className={[classes.buttonLink, classes.buttonsSide].join(' ')}
+                    href="download"
+                >
+                    <Button color="inherit">Download</Button>
+                </Link>
+                <Link
+                    underline="none"
+                    className={[classes.buttonLink, classes.buttonsSide].join(' ')}
+                    href="explore"
+                >
+                    <Button color="inherit">Explore</Button>
+                </Link>
             </Toolbar>
         </AppBar>
         </div>
