77868f7566af8231649db2c2b6c5e450536d0056
Amanpreet Singh <asg@fb.com>
2019-04-10 21:12:12 -0700

    Add paper to Paper section

--- frontend/src/components/Paper.js
+++ frontend/src/components/Paper.js
@@ -1,17 +1,59 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+import CloudDownload from '@material-ui/icons/CloudDownload';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core';
+
+const styles = (theme) => ({
+    embed: {
+        marginTop: '4em',
+        width: '100%',
+        height: '90vh'
+    },
+    buttonLink: {
+        color: theme['palette']['primary']['contrastText'],
+        textDecoration: 'none'
+    },
+    buttonsSide: {
+        textAlign: 'right'
+    },
+
+    leftIcon: {
+        marginRight: theme.spacing.unit
+    }
+});
 
 const Paper = (props) => {
     return (
         <Grid container justify="center" alignItems="center">
-            <Grid item xs={12}>
-                <Typography variant="subtitle1">
-                Paper is coming out soon.
-                </Typography>
+            <Grid
+                item
+                xs={12}
+                md={10}
+                lg={8}
+                style={{
+                    marginTop: '4em'
+                }}
+            >
+                <Link
+                    underline="none"
+                    className={[props.classes.buttonLink, props.classes.buttonsSide].join(' ')}
+                    href="assets/paper/TextVQA.pdf"
+                >
+                    <Button variant="contained" color="primary">
+                        <CloudDownload className={props.classes.leftIcon} />
+                        Download Paper
+                    </Button>
+                </Link>
+                <embed
+                    className={props.classes.embed}
+                    src="assets/paper/TextVQA.pdf"
+                    type="application/pdf"
+                />
             </Grid>
         </Grid>
     )
 }
 
-export default Paper;
+export default withStyles(styles)(Paper);
