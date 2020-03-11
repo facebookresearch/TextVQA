2c8b938b8a72f74f2f5fad3d6391794bf0e6fda9
Amanpreet Singh <asg@fb.com>
2020-03-11 12:37:41 -0700

    Updates

---
+++ precache-manifest.667fcbde82fbd3434ba448d17e4fc41e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea83e44118622533f75f",
+    "url": "/static/css/main.3ef25ec7.chunk.css"
+  },
+  {
+    "revision": "ea83e44118622533f75f",
+    "url": "/static/js/main.2bd5183b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "43e0db63c12db1866bf3",
+    "url": "/static/js/2.d252ff90.chunk.js"
+  },
+  {
+    "revision": "3674051c9edf9b87997691d912cb587e",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c91b9bc8167ac02cbea2f5b0fd468d22.js"
+  "/precache-manifest.667fcbde82fbd3434ba448d17e4fc41e.js"
 );
 
 workbox.clientsClaim();
