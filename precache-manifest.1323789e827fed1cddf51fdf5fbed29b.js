0002cfcc1c8321e59d2be0d039fe47ec0f9d91bb
Amanpreet Singh <asg@fb.com>
2020-03-27 01:21:16 -0700

    Updates

---
+++ precache-manifest.1323789e827fed1cddf51fdf5fbed29b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8f51bcadb0d614ecca6",
+    "url": "/static/css/main.3ef25ec7.chunk.css"
+  },
+  {
+    "revision": "c8f51bcadb0d614ecca6",
+    "url": "/static/js/main.40ae770a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "aaa8735afdc0beb7b86d",
+    "url": "/static/js/2.3a99e186.chunk.js"
+  },
+  {
+    "revision": "a9de731f6f54197bfb234d0a3d034740",
+    "url": "/index.html"
+  }
+];
