24848f0ab24342ef03f3bdc85b8a38e92ae30b3b
Amanpreet Singh <asg@fb.com>
2020-04-19 17:12:46 -0700

    Updates

--- precache-manifest.d8a960ac2b3fccf7a04ef533be521478.js
+++ precache-manifest.d8a960ac2b3fccf7a04ef533be521478.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = [
   {
-    "revision": "8f0407346aa7346fec8b",
-    "url": "/static/css/main.3ef25ec7.chunk.css"
-  },
-  {
-    "revision": "8f0407346aa7346fec8b",
-    "url": "/static/js/main.b19cb69e.chunk.js"
-  },
-  {
     "revision": "42ac5946195a7306e2a5",
     "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "306d995524496386de4e",
+    "url": "/static/js/main.28fa5ef2.chunk.js"
   },
   {
     "revision": "18270b30f50775301156",
     "url": "/static/js/2.7e5675a5.chunk.js"
   },
   {
-    "revision": "4f226c52f5d79bd2b69f249881fb2635",
+    "revision": "306d995524496386de4e",
+    "url": "/static/css/main.3ef25ec7.chunk.css"
+  },
+  {
+    "revision": "ed34ae82105b4c1fe8d5d18bbfc32e38",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b657c94e24b56edca4473afefd506183.js"
+  "/precache-manifest.d8a960ac2b3fccf7a04ef533be521478.js"
 );
 
 workbox.clientsClaim();
